3aae5d3589a99dd34f36e0bd62de0f94213d48c4
Peter <guitaronline89@gmail.com>
2024-12-03 22:22:30 +0200

    initial commit

---
+++ src/main/java/basicSyntaxExercise/PrintAndSum_04.java
@@ -0,0 +1,10 @@
+package basicSyntaxExercise;
+
+import java.util.Scanner;
+
+public class PrintAndSum_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
