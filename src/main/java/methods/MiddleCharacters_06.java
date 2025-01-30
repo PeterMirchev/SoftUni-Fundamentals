6d02e9cd9e35a78d3b29149aa348a1307da54b73
Peter <guitaronline89@gmail.com>
2025-01-30 22:13:18 +0200

    adding comments

--- src/main/java/methods/MiddleCharacters_06.java
+++ src/main/java/methods/MiddleCharacters_06.java
@@ -6,22 +6,29 @@ public class MiddleCharacters_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        String text = scanner.nextLine();
 
-        printMiddleCharacter(input);
+        printMiddleCharacter(text);
     }
 
-    public static void printMiddleCharacter(String input) {
+    public static void printMiddleCharacter(String text) {
 
-        int length = input.length();
-        int index = input.length() / 2;
+        //aString -> 'r' -> index 3 -> length 7
+        //Car -> 'a' -> index 1 -> length 7
+        //Carts -> 'r' -> index 2 -> length 5
 
+        // взимам дължината на текста
+        int length = text.length();
+        // деля целочислено на 2 дължината за да намеря индекса на чара в средата
+        int index = text.length() / 2;
+
+        //ако дължината е четна имаме 2 срредни чара
         if (length % 2 == 0) {
-            char first = input.charAt(index);
-            char second = input.charAt(index - 1);
+            char first = text.charAt(index);
+            char second = text.charAt(index - 1);
             System.out.println("" + second + first);
         } else {
-            char symbol = input.charAt(index);
+            char symbol = text.charAt(index);
             System.out.println("" + symbol);
         }
     }
